b6eed28508c44b53841958ebd8291a6bc25fc6af
Irán Medina Aguilar <125601635+iranmedina04@users.noreply.github.com>
2024-04-08 22:56:50 -0600

    Update MqttSub.py

--- MqttSub.py
+++ MqttSub.py
@@ -17,8 +17,4 @@ mqttc.on_message = on_message
 
 mqttc.connect("test.mosquitto.org", 1883, 60)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 mqttc.loop_forever()
