94be99ab434115d7c39b95bba530e8e55d92d010
Irán Medina Aguilar <125601635+iranmedina04@users.noreply.github.com>
2024-04-08 22:56:25 -0600

    Update MqttPub.py

--- MqttPub.py
+++ MqttPub.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client  as mqtt
 
-client = mqtt.Client()
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.connect("test.mosquitto.org", 1883, 60)
 client.publish("ALSW/temp", "99")
