e53fb739017d6b8b99b066c398bddca8afa53f57
Irán Medina Aguilar <125601635+iranmedina04@users.noreply.github.com>
2024-04-09 00:37:27 -0600

    Update MqttSub.py

--- MqttSub.py
+++ MqttSub.py
@@ -2,13 +2,10 @@ import paho.mqtt.client as mqtt
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
-    print(f"Connected with result code {reason_code}")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("ALSW/#")
-
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
+    print(f"Conexion realizada con exito{reason_code}")
+    client.subscribe("ALSW/#") # cCambie el parametro al topico que desea escuchar. Para el chat grupal puede cambiarlo por el nombre de su companero
+    
+def on_message(client, userdata, msg): #Inprime el mensaje 
     print(msg.topic+" "+str(msg.payload))
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
